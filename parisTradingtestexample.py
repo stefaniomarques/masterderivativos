5c5e7f3f0a31d40613b328b1f4b98eac18033420
Stefanio Marques <stefanioa22@gmail.com>
2023-09-29 21:22:15 -0300

    aula20230929

--- parisTradingtestexample.py
+++ parisTradingtestexample.py
@@ -7,9 +7,9 @@ from arch.unitroot import PhillipsPerron
 from statsmodels.regression.linear_model import OLS
 import statsmodels.api as sm
 
-acoes = ["BBDC4.SA", "ITUB4.SA"]
+acoes = ["PETR3.SA", "PETR4.SA"]
 
-start = "2023-01-01"
+start = "2022-01-01"
 end = "2023-08-31"
 
 df = yf.download(acoes, start=start, end=end)['Adj Close']
@@ -19,15 +19,35 @@ df.isna().sum()
 # Trate os dados brutos para preencher os valores ausentes com a média
 df.fillna(df.mean(), inplace=True)
 
+'''
+Passo 1 Testar as duas séries para raiz unitária
+Passo 2 Rodar OLS (Regressão Linear Simples)
+Passo 3 Testar resíduos para tendência
+'''
+
 # Extrai do data frame uma coluna com iloc
 acao1 = df.iloc[:,0]
 acao1.head()
 acao2 = df.iloc[:,1]
 acao2.head()
 
+#Teste ADF; hipótese nula (H0): A série temporal possui raiz unitária, ou seja, não é estacionária.
+result_adf = sm.tsa.adfuller(acao1)
+print("ADF Test Results:") 
+print(f"Test Statistic: {result_adf[0]}")
+print(f"P-value: {result_adf[1]}")
+print(f"Critical Values: {result_adf[4]}")
+
 #Test Philip-Perron hipótese nula (H0): A série temporal possui uma raiz unitária e é não estacionária.
 pp = PhillipsPerron(acao1)
 print(pp.summary().as_text())
+
+#Teste ADF para ação 02
+result_adf = sm.tsa.adfuller(acao2)
+print("ADF Test Results:") 
+print(f"Test Statistic: {result_adf[0]}")
+print(f"P-value: {result_adf[1]}")
+print(f"Critical Values: {result_adf[4]}")
 
 # Teste PP para ação 02
 pp = PhillipsPerron(acao2)
@@ -45,33 +65,14 @@ residuos = model.resid
 residuos.plot()
 plt.show()
 
-# Teste de raiz unitária dos resíduos (PP)
+# Teste de raiz unitária dos resíduos (ADF E PP)
+result_adf = sm.tsa.adfuller(residuos)
+print("ADF Test Results:") 
+print(f"Test Statistic: {result_adf[0]}")
+print(f"P-value: {result_adf[1]}")
+print(f"Critical Values: {result_adf[4]}")
+
 pp = PhillipsPerron(residuos)
 print(pp.summary().as_text())
 
 # A série é cointegrada se apresentar P-valor abaixo de 0.10
-
-# Apresentar séries no data frame em gráfico
-df.plot()
-plt.show()
-
-# BBDC4 = -04,1373 + 0,7270 * ITUB4 (relação de equilíbrio)
-bbdc4 = 14.30
-itub4 = 27.21
-diferenca = bbdc4 - (-4.1373 + 0.7270 * itub4)
-diferenca
-
-np.std(residuos) # desvio médio dos resíduos
-max(residuos) # diferença máxima da média dos resíduos em módulo
-min(residuos) # diferença mínima da média dos resíduos em módulo
-
-'''
-Processo Ornstein–Uhlenbeck
-Teste de contagem para saber quantas vezes os pares cruzaram 0.00
-Prepapar o loop para os outros ativos do IBOV
-'''
-
-# Calcular diferentes pares de ativos para uma série do IBOV
-n_assets = 85
-x = np.math.factorial(n_assets) / (np.math.factorial(n_assets - 2) * 2)
-xyp-jani-qpk
